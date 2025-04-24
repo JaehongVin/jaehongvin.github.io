70760e980d7e0a3e9c66b0af50e898116c475edb
JaeHong <dnflwoghddl@gmail.com>
2025-04-25 03:34:12 +0900

    chore: package.json 스크립트 추가 및 ignore 추가

--- apps/foot-print/app/page.tsx
+++ apps/foot-print/app/page.tsx
@@ -1,3 +1,11 @@
+import { Test } from '@common/ui/Test';
+import { Test2 } from '@common/ui/Test2';
+
 export default function HomePage() {
-  return <section>해보자</section>;
+  return (
+    <section>
+      <Test2 />
+      <Test />
+    </section>
+  );
 }
