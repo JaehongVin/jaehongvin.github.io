4d6a995caea34d95caec20c2339f6e156efcaf86
JaeHong <dnflwoghddl@gmail.com>
2025-05-15 02:27:19 +0900

    chore: font-weight, font-size, line-height, radius 관련 tailwind css 커스터마이징 theme 추가

--- packages/ui/src/components/Test2.tsx
+++ packages/ui/src/components/Test2.tsx
@@ -1,7 +1,9 @@
 export const Test2 = () => {
   return (
-    <div className="flex-center size-500 border border-gray-500">
-      <p className="font-test-">야호</p>
+    <div className="flex-center size-500 rounded-20 border border-gray-500">
+      <p className="font-400 text-20">
+        야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호야호
+      </p>
     </div>
   );
 };
