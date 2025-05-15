3be49ef9633b3b3e73d75ad88d921fbe7e01f2bd
JaeHong <dnflwoghddl@gmail.com>
2025-05-16 01:28:37 +0900

    feat(donation): 기본 레이아웃 구성 및 스타일 적용

---
+++ apps/foot-print/src/app/(home)/page.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <article className="flex flex-col py-20">메인 홈</article>;
+}
--- apps/foot-print/src/app/layout.tsx
+++ apps/foot-print/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import { cn } from '@common/ui/lib/utils';
 
 export default function GlobalLayout({
   children,
@@ -6,9 +7,19 @@ export default function GlobalLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body>
-        <main>{children}</main>
+    <html lang="ko" className="size-full">
+      <body className="flex size-full bg-gray-100 bg-[url('/assets/images/global-bg.webp')]">
+        <aside className={cn('hidden', 'dt:flex dt:flex-1')}>
+          {/** 좌측 사이드 임시 */}
+        </aside>
+        <main
+          className={cn('hide-scrollbar size-full overflow-y-auto', 'dt:w-900')}
+        >
+          {children}
+        </main>
+        <aside className={cn('hidden', 'dt:flex dt:flex-1')}>
+          {/** 우측 사이드 임시 */}
+        </aside>
       </body>
     </html>
   );
