9ef82e24f9a00cc570fd5bb958c5385d685bd415
JaeHong <dnflwoghddl@gmail.com>
2026-02-04 23:43:23 +0900

    chore: ESLint, Prettier 설정 제거 및 Biome로 마이그레이션

--- apps/foot-print/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import { config } from '@common/eslint-config/next-js';
-
-export default [
-  //* .next 빌드 폴더 무시
-  { ignores: ['.next/**'] },
-
-  //* next.js 관련 설정
-  ...config,
-];
--- packages/ui/eslint.config.mjs
+++
@@ -1,6 +0,0 @@
-import { config } from '@common/eslint-config/react';
-
-export default [
-  //* react.js 관련 설정
-  ...config,
-];
