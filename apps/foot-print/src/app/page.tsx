253e069e5bf85bac59b8610cbcd81da835ca438d
Buddy(빈재홍) <jaehong@wello.info>
2026-02-12 19:11:53 +0900

    comment: root 페이지 주석 추가

--- apps/foot-print/src/app/page.tsx
+++ apps/foot-print/src/app/page.tsx
@@ -6,6 +6,7 @@ import { useLayoutEffect } from 'react';
 const HomePage = () => {
   const router = useRouter();
 
+  //* 포스팅 목록 페이지로 강제 이동
   useLayoutEffect(() => {
     router.replace('/posts');
   }, [router]);
